ebea839359f2cd510937ffda0bfde42735fa85f1
Sandesh Neupane <sandesh.neupane4@gmail.com>
2019-09-08 22:54:14 -0500

    Updates

---
+++ precache-manifest.07fba995a02a2e6360a6edc8a903b898.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec989516fd93c96b5db82cdee5ab4bfd",
+    "url": "/sandesh/index.html"
+  },
+  {
+    "revision": "d2c4efb9edf47a59ef79",
+    "url": "/sandesh/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "be9d01bdb119cde6ddc6",
+    "url": "/sandesh/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "d2c4efb9edf47a59ef79",
+    "url": "/sandesh/static/js/2.6b2c380d.chunk.js"
+  },
+  {
+    "revision": "be9d01bdb119cde6ddc6",
+    "url": "/sandesh/static/js/main.689897d5.chunk.js"
+  },
+  {
+    "revision": "c447d2a480026574c452",
+    "url": "/sandesh/static/js/runtime~main.8e5a97e3.js"
+  }
+]);
